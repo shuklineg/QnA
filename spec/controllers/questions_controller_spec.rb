b3ee6090d24cb82ef3a3cc8f161788add6671453
Evgenii Shuklin <shuklineg@gmail.com>
2019-06-12 17:58:29 +0500

    Use DecentExposure

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,56 +3,6 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
 
-  describe 'GET #index' do
-    let(:questions) { create_list(:question, 3) }
-
-    before { get :index }
-
-    it 'populates an array of all questions' do
-      expect(assigns(:questions)).to match_array(questions)
-    end
-
-    it 'rendes index view' do
-      expect(response).to render_template :index
-    end
-  end
-
-  describe 'GET #show' do
-    before { get :show, params: { id: question } }
-
-    it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
-
-    it 'renders show view' do
-      expect(response).to render_template :show
-    end
-  end
-
-  describe 'GET #new' do
-    before { get :new }
-
-    it 'assigns a new Question to @question' do
-      expect(assigns(:question)).to be_a_new(Question)
-    end
-
-    it 'renders new view' do
-      expect(response).to render_template :new
-    end
-  end
-
-  describe 'GET #edit' do
-    before { get :edit, params: { id: question } }
-
-    it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
-    end
-
-    it 'renders edit view' do
-      expect(response).to render_template :edit
-    end
-  end
-
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves a new question in the database' do
@@ -62,7 +12,7 @@ RSpec.describe QuestionsController, type: :controller do
       it 'redirects to show view' do
         post :create, params: { question: attributes_for(:question) }
 
-        expect(response).to redirect_to assigns(:question)
+        expect(response).to redirect_to controller.question
       end
     end
 
@@ -84,7 +34,7 @@ RSpec.describe QuestionsController, type: :controller do
       it 'assigns the requested question to @question' do
         patch :update, params: { id: question, question: attributes_for(:question) }
 
-        expect(assigns(:question)).to eq question
+        expect(controller.question).to eq question
       end
 
       it 'change question attributes' do
