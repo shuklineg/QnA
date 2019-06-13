82b415402589d80fce375f51cd36e2987019baf8
Evgenii Shuklin <shuklineg@gmail.com>
2019-06-13 13:50:54 +0500

    Fix answers_controller_spec.rb

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe AnswersController, type: :controller do
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves a new answer in the database' do
-        expect { post :create, params: { answer: attributes_for(:answer), question_id: question.id } }.to change(Answer, :count).by(1)
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(question.answers, :count).by(1)
       end
 
       it 'redirects to show view' do
-        post :create, params: { answer: attributes_for(:answer), question_id: question.id }
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
 
         expect(response).to redirect_to question
       end
@@ -18,11 +18,11 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with invalid attributes' do
       it 'does not save the answer' do
-        expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question.id } }.to_not change(Answer, :count)
+        expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question } }.to_not change(question.answers, :count)
       end
 
       it 're-renders new view' do
-        post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question.id }
+        post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question }
 
         expect(response).to render_template :new
       end
