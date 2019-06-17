c9fcfb9bdbf222a738435b9493382632ac25e617
Evgenii Shuklin <shuklineg@gmail.com>
2019-06-17 12:55:23 +0500

    Question index spec

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,10 +1,23 @@
 FactoryBot.define do
+  sequence :title do |n|
+    "Question tilte #{n}"
+  end
+
+  sequence :body do |n|
+    "Question body #{n}"
+  end
+
   factory :question do
     title { "MyString" }
     body { "MyString" }
 
     trait :invalid do
       title { nil }
+    end
+
+    trait :sequences do
+      title
+      body
     end
   end
 end
---
+++ spec/features/questions/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User can create question', %q(
+  In order to find the question
+  As an any user
+  I'd like to be able to see questions list
+) do
+  given!(:questions) { 5.times.collect { create(:question, :sequences) } }
+
+  scenario 'Any user visits questions/index' do
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
+  end
+end
