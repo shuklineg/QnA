73dc49065ba58e7d8c02783066023c158abf60e0
Evgenii Shuklin <shuklineg@gmail.com>
2019-06-18 16:51:51 +0500

    Fix user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def author_of?(resource)
-    resource && resource.user_id == id
+    resource&.user_id == id
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,32 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'User#author_of?' do
+    let(:user) { create(:user) }
+
+    context 'resource authored by the user' do
+      let(:question) { create(:question, user: user) }
+      let(:answer) { create(:answer, user: user, question: question) }
+
+      it 'question' do
+        expect(user.author_of?(question)).to eq true
+      end
+      it 'answer' do
+        expect(user.author_of?(answer)).to eq true
+      end
+    end
+
+    context 'resource authored by another user' do
+      let(:question) { create(:question, user: create(:user)) }
+      let(:answer) { create(:answer, user: create(:user), question: question) }
+
+      it 'question' do
+        expect(user.author_of?(question)).to eq false
+      end
+      it 'answer' do
+        expect(user.author_of?(answer)).to eq false
+      end
+    end
+  end
 end
