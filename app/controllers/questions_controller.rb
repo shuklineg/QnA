7b8af99f9f74e6176d06aeb204cf6710ec2a8682
Evgenii Shuklin <shuklineg@gmail.com>
2019-06-23 12:40:14 +0500

    Attach one file to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -31,6 +31,6 @@ class QuestionsController < ApplicationController
   private
 
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, :file)
   end
 end
