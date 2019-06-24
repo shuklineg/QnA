d770f9939444f17ae0be73049275ef56173bedd5
Evgenii Shuklin <shuklineg@gmail.com>
2019-06-24 12:31:46 +0500

    Edit answer attach file

--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -37,6 +37,18 @@ feature 'User can edit his answer', %q(
       end
     end
 
+    scenario 'add files' do
+      within "#answer-#{answer.id}" do
+        click_on 'Edit'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
+
     scenario 'edit his answer with errors' do
       within "#answer-#{answer.id}" do
         click_on 'Edit'
